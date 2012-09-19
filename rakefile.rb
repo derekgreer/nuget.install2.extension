8bec2822db86606e74eaefbba66c18022eac9a79
Derek Greer <dbgreer@gmail.com>
2012-09-19 09:35:19 -0500

    Added publish target

--- rakefile.rb
+++ rakefile.rb
@@ -14,49 +14,54 @@ FEEDS = ["https://go.microsoft.com/fwlink/?LinkID=206669" ]
 load "VERSION.txt"
 require './packages.rb'
 
-task :default => ["build:all"]
+task :default => ["all"]
 
-namespace :build do
+task :all => [:clean, :dependencies, :compile, :package]
 
-	task :all => [:clean, :dependencies, :compile, :package]
+assemblyinfo :versioning do |asm|
+	asm.output_file = "src/CommonAssemblyInfo.cs"
+ 	asm.version = "#{BUILD_VERSION}"
+end
 
-	assemblyinfo :versioning do |asm|
-  		asm.output_file = "src/CommonAssemblyInfo.cs"
-  		asm.version = "#{BUILD_VERSION}"
+task :clean do
+	rm_rf "#{BUILD_PATH}"
+	rm_rf "#{ARTIFACTS_PATH}"
+end
+
+task :compile => [:versioning] do
+
+	mkdir "#{BUILD_PATH}"
+	sh "#{FRAMEWORK_PATH}msbuild.exe /p:Configuration=#{COMPILE_TARGET} #{SOLUTION}"
+	copyOutputFiles "src/nuget.install2.extension/bin/#{COMPILE_TARGET}", "*.{exe,dll,pdb}", "#{BUILD_PATH}"
+end
+
+task :package do
+	mkdir_p "#{ARTIFACTS_PATH}"
+	rm Dir.glob("#{ARTIFACTS_PATH}/*.nupkg")
+	FileList["packaging/nuget/*.nuspec"].each do |spec|
+	sh "nuget pack #{spec} -o #{ARTIFACTS_PATH} -Version #{BUILD_VERSION} -BasePath ."
 	end
+end
 
-	task :clean do
-		rm_rf "#{BUILD_PATH}"
-		rm_rf "#{ARTIFACTS_PATH}"
-	end
+task :publish => [:all] do
+    FileList["#{ARTIFACTS_PATH}/*.nupkg"].gsub(File::SEPARATOR,
+     File::ALT_SEPARATOR || File::SEPARATOR).each do | file |
+        sh "nuget push #{file}"
+    end
+end
 
-	task :compile => [:versioning] do
 
-		mkdir "#{BUILD_PATH}"
-		sh "#{FRAMEWORK_PATH}msbuild.exe /p:Configuration=#{COMPILE_TARGET} #{SOLUTION}"
-		copyOutputFiles "src/nuget.install2.extension/bin/#{COMPILE_TARGET}", "*.{exe,dll,pdb}", "#{BUILD_PATH}"
-	end
-
-	task :package do
-		mkdir_p "#{ARTIFACTS_PATH}"
-		rm Dir.glob("#{ARTIFACTS_PATH}/*.nupkg")
-		FileList["packaging/nuget/*.nuspec"].each do |spec|
-		sh "nuget pack #{spec} -o #{ARTIFACTS_PATH} -Version #{BUILD_VERSION} -BasePath ."
+task :dependencies do
+	configatron.packages.each do | package |
+		FEEDS.each do | feed | 
+			!(File.exists?("#{LIB_PATH}/#{package[0]}")) and
+				sh "nuget Install #{package[0]} -Version #{package[1]} -o #{LIB_PATH} -Source #{feed} -ExcludeVersion" do | cmd, results | cmd  end
 		end
 	end
+end
 
-	task :dependencies do
-		configatron.packages.each do | package |
-			FEEDS.each do | feed | 
-				!(File.exists?("#{LIB_PATH}/#{package[0]}")) and
-					sh "nuget Install #{package[0]} -Version #{package[1]} -o #{LIB_PATH} -Source #{feed} -ExcludeVersion" do | cmd, results | cmd  end
-			end
-		end
-	end
-
-	def copyOutputFiles(fromDir, filePattern, outDir)
-		Dir.glob(File.join(fromDir, filePattern)){|file| 		
-			copy(file, outDir) if File.file?(file)
-  		} 
-	end
+def copyOutputFiles(fromDir, filePattern, outDir)
+	Dir.glob(File.join(fromDir, filePattern)){|file| 		
+		copy(file, outDir) if File.file?(file)
+  	} 
 end
